09e4d93271261552460a95bc14bad069263c50cf
Henry <46249787+MiHenri@users.noreply.github.com>
2020-07-10 09:36:13 -0700

    Update
    Update

--- src/components/Amsterdam.js
+++ src/components/Amsterdam.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Navbar from './Navbar';
+import Navbar from './layouts/Navbar';
 import "../App.css";
 
 class Amsterdam extends Component {
@@ -8,33 +8,33 @@ class Amsterdam extends Component {
       return(
         <React.Fragment>
         <Navbar />
-      //  <div className='App'>
-      //     <nav className='navbar a bottom-border '>     
-      //       <ul>
-      //         <li>
-      //           <a href='#'class='' >Cupertino</a>
-      //         </li>
-      //         <li>
-      //           <a href='#'class='' >New York City</a>
-      //         </li>
-      //         <li>
-      //           <a href='#'class='' >London</a>
-      //         </li>
-      //         <li>
-      //           <a href='#'class='city'>Amsterdam</a>
-      //         </li>
-      //         <li>
-      //           <a href='#'class=''>Tokyo</a>
-      //         </li>
-      //         <li>
-      //           <a href='#'class=''>Hong Kong</a>
-      //         </li>
-      //         <li>
-      //           <a href='#'class=''>Sydney</a>
-      //         </li>
-      //       </ul>
-      //     </nav>
-      //  </div>
+       <div className='App'>
+          <nav className=' '>     
+            <ul>
+              <li>
+                <a href='#'class='' >Cupertino</a>
+              </li>
+              <li>
+                <a href='#'class='' >New York City</a>
+              </li>
+              <li>
+                <a href='#'class='' >London</a>
+              </li>
+              <li>
+                <a href='#'class='navbar'>Amsterdam</a>
+              </li>
+              <li>
+                <a href='#'class=''>Tokyo</a>
+              </li>
+              <li>
+                <a href='#'class=''>Hong Kong</a>
+              </li>
+              <li>
+                <a href='#'class=''>Sydney</a>
+              </li>
+            </ul>
+          </nav>
+       </div>
         
         </React.Fragment>
      );
