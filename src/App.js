132ee7f046fbad3b6fcae288fc3e664182444257
Henry <46249787+MiHenri@users.noreply.github.com>
2020-07-10 10:08:53 -0700

    Update
    Update

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ class App extends Component {
        <div className='App'>
           <nav className='navbar'>     
             <li>
-                <NavLink exact className="main-nav " activeClassName="main-nav-active" to='./Cupertino.js'>Cupertino</NavLink>
-                <NavLink exact className="main-nav " activeClassName="main-nav-active" to='./NewYorkCity.js'>New York City</NavLink>
-                <NavLink exact className="main-nav " activeClassName="main-nav-active" to='./London.js'>London</NavLink>
-                <NavLink exact className="main-nav " activeClassName="main-nav-active" to='./Amsterdam.js'>Amsterdam</NavLink>
-                <NavLink exact className="main-nav " activeClassName="main-nav-active" to='./Tokyo.js'>Tokyo</NavLink>
-                <NavLink exact className="main-nav " activeClassName="main-nav-active" to='./HongKong.js'>Hong Kong</NavLink>
-                <NavLink exact className="main-nav " activeClassName="main-nav-active" to='./Sydney.js'>Sydney</NavLink>
+                <NavLink exact className="main-nav" activeClassName="main-nav-active line" to='./Cupertino.js'>Cupertino</NavLink>
+                <NavLink exact className="main-nav" activeClassName="main-nav-active line" to='./NewYorkCity.js'>New York City</NavLink>
+                <NavLink exact className="main-nav" activeClassName="main-nav-active line" to='./London.js'>London</NavLink>
+                <NavLink exact className="main-nav" activeClassName="main-nav-active line" to='./Amsterdam.js'>Amsterdam</NavLink>
+                <NavLink exact className="main-nav" activeClassName="main-nav-active line" to='./Tokyo.js'>Tokyo</NavLink>
+                <NavLink exact className="main-nav" activeClassName="main-nav-active line" to='./HongKong.js'>Hong Kong</NavLink>
+                <NavLink exact className="main-nav" activeClassName="main-nav-active line" to='./Sydney.js'>Sydney</NavLink>
             </li>
           </nav>
        </div>
