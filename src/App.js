317787453da7310ac1ffc425f033bdc9c3cd2e45
Henry <46249787+MiHenri@users.noreply.github.com>
2020-07-10 10:46:13 -0700

    Update
    tweaks

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ class App extends Component {
        <div className='App'>
           <nav className='navbar'>     
             <li>
-                <NavLink exact className="main-nav" activeClassName="main-nav-active line" to='./Cupertino.js'>Cupertino</NavLink>
-                <NavLink exact className="main-nav" activeClassName="main-nav-active line" to='./NewYorkCity.js'>New York City</NavLink>
-                <NavLink exact className="main-nav" activeClassName="main-nav-active line" to='./London.js'>London</NavLink>
-                <NavLink exact className="main-nav" activeClassName="main-nav-active line" to='./Amsterdam.js'>Amsterdam</NavLink>
-                <NavLink exact className="main-nav" activeClassName="main-nav-active line" to='./Tokyo.js'>Tokyo</NavLink>
-                <NavLink exact className="main-nav" activeClassName="main-nav-active line" to='./HongKong.js'>Hong Kong</NavLink>
-                <NavLink exact className="main-nav" activeClassName="main-nav-active line" to='./Sydney.js'>Sydney</NavLink>
+                <NavLink exact className="main-nav" activeClassName="main-nav-active line" to='./Cupertino.js' activeStyle={{color: 'black'}}>Cupertino</NavLink>
+                <NavLink exact className="main-nav" activeClassName="main-nav-active line" to='./NewYorkCity.js'activeStyle={{color: 'black'}}>New York City</NavLink>
+                <NavLink exact className="main-nav" activeClassName="main-nav-active line" to='./London.js'activeStyle={{color: 'black'}}>London</NavLink>
+                <NavLink exact className="main-nav" activeClassName="main-nav-active line" to='./Amsterdam.js'activeStyle={{color: 'black'}}>Amsterdam</NavLink>
+                <NavLink exact className="main-nav" activeClassName="main-nav-active line" to='./Tokyo.js'activeStyle={{color: 'black'}}>Tokyo</NavLink>
+                <NavLink exact className="main-nav" activeClassName="main-nav-active line" to='./HongKong.js'activeStyle={{color: 'black'}}>Hong Kong</NavLink>
+                <NavLink exact className="main-nav" activeClassName="main-nav-active line" to='./Sydney.js'activeStyle={{color: 'black'}}>Sydney</NavLink>
             </li>
           </nav>
        </div>
